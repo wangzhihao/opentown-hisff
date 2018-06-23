b8ef1be25b8d9b4456916f4bb7ae30ba6c4a3864
zhihao <zhihaow@amazon.com>
2018-06-24 00:20:56 +0800

    add "|" separator

--- hisff/src/hisff/hisff-menu.js
+++ hisff/src/hisff/hisff-menu.js
@@ -6,13 +6,15 @@ import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import '@polymer/paper-styles/color.js';
 import '@polymer/paper-styles/typography.js';
 
+import 'underscore/underscore-min.js';
+
 
 Polymer({
   _template: html`
   <custom-style>
     <style>
       .menu-bar {
-        --menu-padding: 5px;
+        --menu-padding: 7px;
       }
 
       .menu-bar {
@@ -29,7 +31,13 @@ Polymer({
         background-color: var(--paper-grey-900);
         padding: 0px var(--menu-padding);
       }
-
+      .menu-bar a.menu-item.separator:after {
+        content: '|';
+        font-size: 1.2em;
+        vertical-align: middle;
+        position: relative;
+        left: var(--menu-padding);
+      }
       .menu-bar a.menu-item.right {
         float: right;
         padding: 0 var(--menu-padding);
@@ -40,7 +48,7 @@ Polymer({
   <div class="menu-bar">
     <dom-repeat items="[[items]]">
       <template>
-        <a href="[[item.link]]" class$="menu-item [[item.direction]]">[[item.name]]</a>
+        <a href="[[item.link]]" class$="menu-item [[item.direction]] [[_should_decorate_with_separator(item)]]">[[item.name]]</a>
       </template>
     </dom-repeat>
   </div>
@@ -87,6 +95,13 @@ Polymer({
         }
       ]
     }
+  },
+  /**
+   * Decorate all left menu items except the last one with '|'.
+   */
+  _should_decorate_with_separator: function(item) {
+      const [lastLeftMenuItem] = _.filter(this.items, function(i){return i.direction != 'right';}).slice(-1);
+      return lastLeftMenuItem.name != item.name && item.direction != 'right' ? 'separator' : '';
   },
   is: 'hisff-menu'
 });
